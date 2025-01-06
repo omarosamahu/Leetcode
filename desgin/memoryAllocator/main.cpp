77dd778074121662e29b99608f7cb69d14e506d7
omarosamahu <omarosama255@gmail.com>
2025-01-06 19:35:49 +0200

    msg

---
+++ desgin/memoryAllocator/main.cpp
@@ -0,0 +1,21 @@
+#include "memory_allocator.h"
+
+bool getArgs(char const *args[])
+{
+    return (strcmp(args[1], "-consecutive") == 0) ? true : (strcmp(args[1], "-freeIndeces") == 0) ? false
+                                                                                                  : throw std::invalid_argument("Invalid Argument");
+}
+int main(int argc, char const *argv[])
+{
+    auto allocatorPtr = std::make_unique<Allocator>(10);
+
+    allocatorPtr->allocate(1, 1, getArgs(argv));
+    allocatorPtr->allocate(1, 2, getArgs(argv));
+    allocatorPtr->allocate(1, 3, getArgs(argv));
+    allocatorPtr->freeMemory(2);
+    allocatorPtr->allocate(3, 4, getArgs(argv));
+    allocatorPtr->allocate(1, 1, getArgs(argv));
+    allocatorPtr->allocate(1, 1, getArgs(argv));
+    allocatorPtr->print();
+    return 0;
+}
