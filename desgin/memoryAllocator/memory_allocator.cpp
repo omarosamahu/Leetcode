f5da8db5638d958cb626846b17fccbae6a0626c2
omarosamahu <omarosama255@gmail.com>
2025-01-06 19:44:14 +0200

    Removing binaries

---
+++ desgin/memoryAllocator/memory_allocator.cpp
@@ -0,0 +1,128 @@
+#include "memory_allocator.h"
+
+Allocator::Allocator(uint16_t n) : m_array(new int[n]), array_length{n}, idx{0}
+{
+    std::fill(m_array, m_array + n, INT_MAX);
+    for (int i = 0; i < n; ++i)
+    {
+        freeIndeces[i] = true;
+    }
+}
+
+Allocator::~Allocator()
+{
+    if (m_array != nullptr)
+    {
+        delete[] m_array;
+    }
+}
+
+int Allocator::allocate(int size, int id, bool isConsecutive)
+{
+    if ((idx + size) >= array_length)
+    {
+        std::cerr << "Insufficient memory\n";
+        return -1;
+    };
+
+    return isConsecutive ? appendConsecutive(size, id) + 1 : addToFreeIndeces(size, id);
+}
+
+int Allocator::getFreeSlots(int size)
+{
+    int count = 0;
+    int pidx;
+    for (size_t i = 0; i < array_length; i++)
+    {
+        if (freeIndeces[i])
+        {
+            count++;
+            if (count == size)
+            {
+                pidx = i;
+                break;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+    return pidx;
+}
+
+int Allocator::freeMemory(int id)
+{
+    int freeCount = 0;
+    for (int i = 0; i < array_length; ++i)
+    {
+        if (m_array[i] == id)
+        {
+            m_array[i] = INT_MAX;
+            freeIndeces[i] = true;
+            ++freeCount;
+            --idx;
+        }
+    }
+    return freeCount;
+}
+
+void Allocator::print()
+{
+    for (size_t i = 0; i < array_length; i++)
+    {
+        if (i < (array_length - 1))
+        {
+            std::cout << m_array[i] << ",";
+        }
+        else
+        {
+            std::cout << m_array[i];
+        }
+    }
+    std::cout << "\n";
+}
+
+void Allocator::displayFree()
+{
+    for (const auto &pair : freeIndeces)
+    {
+        std::cout << "Satrt: " << pair.first << " Second: " << pair.second << "\n";
+    }
+}
+
+int Allocator::addToFreeIndeces(uint32_t size, int id)
+{
+    bool firstHit = false;
+    int pIdx = 0;
+
+    for (size_t i = 0; i < array_length; i++)
+    {
+        if (size > 0 && freeIndeces[i])
+        {
+            if (!firstHit)
+            {
+                pIdx = i;
+                firstHit = true;
+            }
+            m_array[i] = id;
+            freeIndeces[i] = false;
+            --size;
+            ++idx;
+        }
+    }
+    return pIdx;
+}
+
+int Allocator::appendConsecutive(uint32_t size, int id)
+{
+    int endIndex = getFreeSlots(size);
+    while (size > 0)
+    {
+        m_array[endIndex] = id;
+        freeIndeces[endIndex--] = false;
+        size--;
+        idx++;
+    }
+    return endIndex;
+}
---
+++ desgin/memoryAllocator/memory_allocator.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <algorithm>
+
+class Allocator
+{
+public:
+    Allocator() = default;
+    Allocator(uint16_t n);
+    ~Allocator();
+
+    int allocate(int size, int id, bool isConsecutive = true);
+    int freeMemory(int id);
+    int getFreeSlots(int size);
+    void print();
+    void displayFree();
+
+    int addToFreeIndeces(uint32_t size, int id);
+    int appendConsecutive(uint32_t size, int id);
+
+private:
+    int *m_array{nullptr};
+    uint16_t idx;
+    uint16_t array_length;
+    std::unordered_map<int, bool> freeIndeces;
+};
