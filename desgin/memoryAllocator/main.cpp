99458e82bf2a2112c9af0dff4abef29a6134310b
omarosamahu <omarosama255@gmail.com>
2025-01-07 13:20:50 +0200

    Add test50

--- desgin/memoryAllocator/main.cpp
+++ desgin/memoryAllocator/main.cpp
@@ -7,7 +7,60 @@ bool getArgs(char const *args[])
 }
 int main(int argc, char const *argv[])
 {
-    auto allocatorPtr = std::make_unique<Allocator>(10);
+    auto allocatorPtr = std::make_unique<Allocator>(50);
+    allocatorPtr->allocate(12, 6);
+    allocatorPtr->allocate(28, 16);
+    allocatorPtr->allocate(17, 23);
+    allocatorPtr->allocate(50, 23);
+    allocatorPtr->freeMemory(6);
+    allocatorPtr->freeMemory(10);
+    allocatorPtr->freeMemory(10);
+    allocatorPtr->allocate(16, 8);
+    allocatorPtr->allocate(17, 41);
+    allocatorPtr->allocate(44, 27);
+    allocatorPtr->allocate(12, 45);
+    allocatorPtr->freeMemory(33);
+    allocatorPtr->freeMemory(8);
+    allocatorPtr->freeMemory(16);
+    allocatorPtr->freeMemory(23);
+    allocatorPtr->freeMemory(23);
+    allocatorPtr->freeMemory(23);
+    allocatorPtr->freeMemory(29);
+    allocatorPtr->allocate(38, 32);
+    allocatorPtr->freeMemory(29);
+    allocatorPtr->freeMemory(6);
+    allocatorPtr->allocate(40, 11);
+    allocatorPtr->freeMemory(16);
+    allocatorPtr->allocate(22, 33);
+    allocatorPtr->allocate(27, 5);
+    allocatorPtr->freeMemory(3);
+    allocatorPtr->freeMemory(10);
+    allocatorPtr->freeMemory(29);
+    allocatorPtr->allocate(16, 14);
+    allocatorPtr->allocate(46, 47);
+    allocatorPtr->allocate(48, 9);
+    allocatorPtr->allocate(36, 17);
+    allocatorPtr->freeMemory(33);
+    allocatorPtr->allocate(14, 24);
+    allocatorPtr->freeMemory(16);
+    allocatorPtr->freeMemory(8);
+    allocatorPtr->allocate(2, 50);
+    allocatorPtr->allocate(31, 36);
+    allocatorPtr->allocate(17, 45);
+    allocatorPtr->allocate(46, 31);
+    allocatorPtr->allocate(2, 6);
+    allocatorPtr->allocate(16, 2);
+    allocatorPtr->allocate(39, 30);
+    allocatorPtr->freeMemory(33);
+    allocatorPtr->freeMemory(45);
+    allocatorPtr->freeMemory(30);
+    allocatorPtr->freeMemory(27);
+    allocatorPtr->display();
+    return 0;
+}
+
+/*
+auto allocatorPtr = std::make_unique<Allocator>(10);
 
     allocatorPtr->allocate(1, 1, getArgs(argv));
     allocatorPtr->allocate(1, 2, getArgs(argv));
@@ -19,5 +72,4 @@ int main(int argc, char const *argv[])
     allocatorPtr->allocate(2, 6, getArgs(argv));
     allocatorPtr->allocate(1, 7, getArgs(argv));
     allocatorPtr->display();
-    return 0;
-}
+*/
