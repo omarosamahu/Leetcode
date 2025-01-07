5165590b17a9c3f2f85dd52a1d8bbc62b64d2d59
omarosamahu <omarosama255@gmail.com>
2025-01-07 18:30:56 +0200

    Changing id type

--- desgin/memoryAllocator/memory_allocator.h
+++ desgin/memoryAllocator/memory_allocator.h
@@ -15,6 +15,10 @@ public:
     Allocator() = default;
     explicit Allocator(uint16_t n);
     ~Allocator();
+    Allocator(const Allocator &) = delete;
+    Allocator &operator=(const Allocator &) = delete;
+    Allocator(Allocator &&) = delete;
+    Allocator &operator=(Allocator &&) = delete;
 
     int16_t allocate(uint16_t size, int16_t id, bool isConsecutive = true);
     int16_t getFreeSlots(uint16_t size);
